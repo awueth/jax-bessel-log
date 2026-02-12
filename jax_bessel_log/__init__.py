15f1adbc34d1b4b3739b68e709a4b93d405d6b0e
awueth <83429722+awueth@users.noreply.github.com>
2026-02-12 23:16:52 +0100

    jv_log based on callback

--- jax_bessel_log/__init__.py
+++ jax_bessel_log/__init__.py
@@ -1,3 +1,4 @@
 from .iv_log import iv_log
+from .jv_log import jv_log
 
-__all__ = ["iv_log"]
+__all__ = ["iv_log", "jv_log"]
---
+++ jax_bessel_log/jv_log.py
@@ -0,0 +1,26 @@
+import jax
+import jax.numpy as jnp
+import scipy.special
+
+
+def jv_log(v, z):
+  v, z = jnp.asarray(v), jnp.asarray(z)
+
+  # Require the order v to be integer type: this simplifies
+  # the JVP rule below.
+  assert jnp.issubdtype(v.dtype, jnp.integer)
+
+  # Promote the input to inexact (float/complex).
+  # Note that jnp.result_type() accounts for the enable_x64 flag.
+  z = z.astype(jnp.result_type(float, z.dtype))
+
+  # Wrap scipy function to return the expected dtype.
+  _scipy_jv = lambda v, z: jnp.log(scipy.special.jve(v, z).astype(z.dtype)) + z
+
+  # Define the expected shape & dtype of output.
+  result_shape_dtype = jax.ShapeDtypeStruct(
+      shape=jnp.broadcast_shapes(v.shape, z.shape),
+      dtype=z.dtype)
+
+  # Use vmap_method="broadcast_all" because scipy.special.jv handles broadcasted inputs.
+  return jax.pure_callback(_scipy_jv, result_shape_dtype, v, z, vmap_method="broadcast_all")
